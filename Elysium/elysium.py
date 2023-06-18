c7b3422a933adb4fc8c778621b701d9d0ab7fdc3
Kye <kye@apacmediasolutions.com>
2023-06-18 09:53:16 -0400

    no csv stuff

--- Elysium/elysium.py
+++ Elysium/elysium.py
@@ -4,7 +4,7 @@ from typing import List, Union
 
 import dotenv
 import numpy as np
-import oceandb
+# import oceandb
 import pandas as pd
 import pyarrow.parquet as pq
 from oceandb.utils.embedding_functions import ImageBindEmbeddingFunction
@@ -19,56 +19,45 @@ audio_embedding_function = ImageBindEmbeddingFunction(modality="audio")
 
 
 class Elysium:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, file_type: str):
         self.file_path = file_path
+        self.file_type = file_type
         self.df = None
+        self.embedding_function = ImageBindEmbeddingFunction()
 
     @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
-    def get_embedding(self, data: Union[str, Image.Image], data_type: str) -> List[float]:
-        if data_type == 'text':
-            return text_embedding_function.embed(data)
-        elif data_type == 'image':
-            return vision_embedding_function.embed(data)
-        elif data_type == 'audio':
-            return audio_embedding_function.embed(data)
-        elif data_type == 'video':
-            # Implement video embedding generation using an appropriate pre-trained model
-            pass
-        else:
-            raise ValueError(f"Unsupported data type: {data_type}")
-
-    def read_csv_file(self):
-        self.df = pd.read_csv(self.file_path)
-        for index, row in self.df.iterrows():
-            if row['DataType'] in ['image', 'audio', 'video']:
-                file_path = row['Data']
-                if not os.path.exists(file_path):
-                    print(f"File not found: {file_path}")
+    def get_embedding(self, file_path: str) -> List[float]:
+        if self.file_type == 'image':
+            img = Image.open(file_path)
+            return self.embedding_function.embed_image(img)
+        elif self.file_type == 'audio':
+            audio_data, samplerate = sf.read(file_path)
+            return self.embedding_function.embed_audio(audio_data, samplerate)
 
     def generate_embeddings(self):
+        files = os.listdir(self.file_path)
         embeddings = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-            future_to_embedding = {executor.submit(self.get_embedding, row['Data'], row['DataType']): row for _, row in self.df.iterrows()}
+            future_to_embedding = {executor.submit(self.get_embedding, os.path.join(self.file_path, file)): file for file in files}
             for future in concurrent.futures.as_completed(future_to_embedding):
-                embeddings.append(future.result())
-                print(len(embeddings), end='\r')
-        self.df['Embedding'] = embeddings
+                embeddings.append((future_to_embedding[future], future.result()))
+        self.df = pd.DataFrame(embeddings, columns=['File', 'Embedding'])
 
     def save_to_parquet(self):
-        parquet_file_name = self.file_path.replace('.csv', '.parquet')
+        parquet_file_name = self.file_path + '.parquet'
         self.df.to_parquet(parquet_file_name)
 
     def verify_embeddings(self):
-        parquet_file_name = self.file_path.replace('.csv', '.parquet')
+        parquet_file_name = self.file_path + '.parquet'
         table = pq.read_table(parquet_file_name)
         df = table.to_pandas()
         print(df.head())
 
 
 if __name__ == "__main__":
-    file_path = 'path_to_your_file.csv'
-    elysium = Elysium(file_path)
-    elysium.read_csv_file()
+    directory_path = 'path_to_your_directory'
+    file_type = 'audio'  # or 'image'
+    elysium = Elysium(directory_path, file_type)
     elysium.generate_embeddings()
     elysium.save_to_parquet()
     elysium.verify_embeddings()
